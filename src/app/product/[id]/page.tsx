5e2d17045915bfbfc2b0b9ec093e7a2a4c992352
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2025-05-19 14:36:41 +0530

    fixed lint error

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -3,18 +3,18 @@ import { fetchProductDetails } from "@/app/components/fetchProductDetail";
 import React from "react";
 
 interface DetailPageProps {
-  params: {
-    id: string;
-  };
+     params: {
+          id: string;
+     };
 }
 // we need to name the prop as params, if not we will get undefined. This is because next router dynamic file always get props named as
 // -params
 
 const productDetailPage = async ({ params }: DetailPageProps) => {
+     const id = Number(params.id);
+     const data = await fetchProductDetails(id);
 
-     const data = await fetchProductDetails(Number(params.id) );
-
-     return <ProductDetail product={data}/>;
+     return <ProductDetail product={data} />;
 };
 
 export default productDetailPage;
