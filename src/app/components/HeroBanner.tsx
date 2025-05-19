d8d88bcb560f245c77a938df853af2bcb4787a21
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2025-05-19 11:13:22 +0530

    fix: fixed header banner image

--- src/app/components/HeroBanner.tsx
+++ src/app/components/HeroBanner.tsx
@@ -1,22 +1,25 @@
-import Image from "next/image";
+import Image from 'next/image'
+import bannerImage from '../../../public/shopping-online-on-phone-podium-260nw-1886650081.webp'
 
 const HeroBanner = () => (
-     <section className="relative w-full h-[300px] bg-gray-100">
+     <section className="relative w-full h-[350px] bg-gray-100">
           {/* <Image
                width={100}
                height={100}
-               src="https://t3.ftcdn.net/jpg/06/32/90/44/360_F_632904407_iPLi90WdjZ0oKAeRiL98gEIeHIUtzWae.jpg" // Replace with actual path
+               src={bannerImage}
                alt="Hero"
                className="w-full h-full object-cover"
           /> */}
-          <img src="header.jpg" alt="Hero" className="w-full h-full object-cover" />
-          <div className="absolute top-1/2 left-12 transform -translate-y-1/2">
-               <h2 className="text-5xl font-light text-black">
+
+          <img  className="w-full h-full object-fit " src="https://t4.ftcdn.net/jpg/06/22/74/79/360_F_622747997_4s5nw9y2WG3LJyQ5iRF4KRGLbySGRd82.jpg"/>
+       {/* <div className="absolute top-1/2 left-12 transform -translate-y-1/2">
+               <h2 className="text-5xl font-light text-white">
                     Simple
                     <br />
                     is More
                </h2>
-          </div>
+          </div> */}
+         
      </section>
 );
 
